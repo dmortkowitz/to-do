2569abdde3d034e84d9d7f00c73e0e8121282b40
David Mortkowitz <david@>
2018-08-20 11:54:54 -0700

    Add prototype to js object.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,31 @@
 //backend
 
 
+function toDo(itemList) {
+  this.itemList = itemList;
+}
 
+toDo.prototype.removeitemList = function () {
+  $("#toDoOutput li").unbind('click').click(function(){
+    $(this).remove();
+    $("#completed").append("<li>" + $(this).text() + "</li>");
+  })
+
+}
 
 
 //frontend
 $(document).ready(function(){
   $("form#toDoSubmit").submit(function(event){
   event.preventDefault();
-  var listInput = $("#toDoList").val();
-  $("#toDoOutput").append(("<li>" + listInput + "</li>"));
+    var listInput = $("#toDoList").val();
+    var newList = new toDo(listInput);
+    // var
+    $("#toDoList").val();
+    $(".completed").show();
+    $("#toDoOutput").append(("<li>" + listInput + "</li>"));
+    newList.removeitemList();
+
+    });
   });
-});
+// });
